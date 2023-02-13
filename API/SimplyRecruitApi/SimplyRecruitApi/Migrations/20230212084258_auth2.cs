e52f248c12273339adae819d1b0de41759c44029
Rugile Karengaite <rugile.karengaite@ktu.edu>
2023-02-13 18:36:41 +0200

    migrations

---
+++ API/SimplyRecruitApi/SimplyRecruitApi/Migrations/20230212084258_auth2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimplyRecruitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class auth2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "AspNetUsers",
+                newName: "AccessToken");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccessToken",
+                table: "AspNetUsers",
+                newName: "Surname");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
