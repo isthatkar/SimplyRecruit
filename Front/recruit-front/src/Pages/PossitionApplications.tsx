121f3504a2f7031262333c646743e63b3f633996
Rugile Karengaite <rugile.karengaite@ktu.edu>
2023-02-23 19:41:57 +0200

    very ugly table for applications

--- Front/recruit-front/src/Pages/PossitionApplications.tsx
+++ Front/recruit-front/src/Pages/PossitionApplications.tsx
@@ -1,31 +1,229 @@
-import {
-  Box,
-  Container,
-  Stack,
-  ThemeProvider,
-  Typography,
-} from "@mui/material";
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import ApplicationListItem from "../Components/Applications/ApplicationListItem";
+import * as React from "react";
+import { alpha, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
 import Theme from "../Styles/Theme";
-import { Application } from "../Types/types";
+import { Application, Stage } from "../Types/types";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
-const PositionApplications = () => {
-  const [allApplications, setAllApplications] = useState<Application[]>([]);
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Application;
+  label: string;
+  sortable: boolean;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "fullName",
+    numeric: false,
+    sortable: false,
+    disablePadding: true,
+    label: "Full name",
+  },
+  {
+    id: "phoneNumber",
+    numeric: true,
+    sortable: false,
+    disablePadding: false,
+    label: "phone",
+  },
+  {
+    id: "contactEmail",
+    numeric: true,
+    sortable: false,
+    disablePadding: false,
+    label: "contactEmail",
+  },
+  {
+    id: "stage",
+    numeric: true,
+    sortable: true,
+    disablePadding: false,
+    label: "stage",
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Application
+  ) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler =
+    (property: keyof Application) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "center"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            {headCell.sortable ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : "asc"}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === "desc"
+                      ? "sorted descending"
+                      : "sorted ascending"}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            ) : (
+              headCell.label
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+function EnhancedTableToolbar() {
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...{
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        },
+      }}
+    >
+      <Typography
+        sx={{ flex: "1 1 100%" }}
+        variant="h6"
+        id="tableTitle"
+        component="div"
+      >
+        Position applications
+      </Typography>
+
+      <Tooltip title="Filter list">
+        <IconButton>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+    </Toolbar>
+  );
+}
+
+export default function EnhancedTable() {
+  const [allApplications, setAllApplications] = React.useState<Application[]>(
+    []
+  );
   const { positionId } = useParams();
 
-  const getPositionApplications = useCallback(async () => {
+  const getPositionApplications = React.useCallback(async () => {
     const response = await axios.get(`positions/${positionId}/applications`);
     console.log(response.data);
     const applications = response.data;
     setAllApplications(applications);
   }, []);
 
-  useEffect(() => {
+  React.useEffect(() => {
     getPositionApplications();
   }, []);
+
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof Application>("stage");
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(15);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Application
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0
+      ? Math.max(0, (1 + page) * rowsPerPage - allApplications.length)
+      : 0;
+
   return (
     <ThemeProvider theme={Theme}>
       <Box
@@ -33,40 +231,71 @@ const PositionApplications = () => {
           bgcolor: "background.paper",
           pt: 8,
           pb: 6,
+          mx: 5,
         }}
       >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Applications
-          </Typography>
-        </Container>
+        <Paper sx={{ width: "100%", mb: 2 }}>
+          <EnhancedTableToolbar />
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size={"medium"}
+            >
+              <EnhancedTableHead
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                rowCount={allApplications.length}
+              />
+              <TableBody>
+                {stableSort(allApplications, getComparator(order, orderBy))
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) => {
+                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                    return (
+                      <TableRow
+                        hover
+                        tabIndex={-1}
+                        key={row.id}
+                        component={Link}
+                        to={`/application/${row.id}`}
+                        sx={{
+                          textDecoration: "none",
+                          "&:hover": {
+                            textDecoration: "none",
+                          },
+                        }}
+                      >
+                        <TableCell
+                          component="th"
+                          id={labelId}
+                          align="center"
+                          scope="row"
+                          padding="none"
+                        >
+                          {row.positionName} <br></br>
+                          {row.fullName}
+                        </TableCell>
+                        <TableCell align="right">{row.phoneNumber}</TableCell>
+                        <TableCell align="right">{row.contactEmail}</TableCell>
+                        <TableCell align="right">
+                          {Object.values(Stage)[row.stage]}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                {emptyRows > 0 && (
+                  <TableRow>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
       </Box>
-      <Container>
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={1}
-        >
-          {allApplications.map((application) => (
-            <ApplicationListItem
-              key={application.id}
-              email={application.contactEmail}
-              stage={application.stage}
-              fullName={application.fullName}
-              positionName={application.positionName}
-            ></ApplicationListItem>
-          ))}
-        </Stack>
-      </Container>
     </ThemeProvider>
   );
-};
-
-export default PositionApplications;
+}
