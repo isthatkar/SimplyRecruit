05ad81c302ef582b3b5c836f0e354bf81d431fbc
Rugile Karengaite <rugile.karengaite@ktu.edu>
2023-03-01 22:36:49 +0200

    prettier meetings page

--- Front/recruit-front/src/Components/Meetings/MeetingsCalendar.tsx
+++ Front/recruit-front/src/Components/Meetings/MeetingsCalendar.tsx
@@ -6,51 +6,60 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import { EventSourceInput } from "@fullcalendar/core";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Meeting } from "../../Types/types";
+import { Button, Theme, Typography } from "@mui/material";
+import { makeStyles, createStyles } from "@mui/styles";
+import { ThemeProvider } from "@emotion/react";
+import MyTheme from "../../Styles/Theme";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    calendarContainer: {
+      margin: "0 auto",
+      padding: "20px",
+      width: "80%",
+      maxHeight: "5px",
+    },
+    calendarHeader: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginBottom: "20px",
+    },
+  })
+);
 
 function MeetingCalendar(props: any) {
+  const classes = useStyles();
+
   const [meetings, setMeetings] = useState<Meeting[]>([]);
   const [eventSources, setEventSources] = useState<EventSourceInput[]>();
 
   useEffect(() => {
     // fetch meetings from API or local storage
     const storedMeetings = props.meetings;
+    console.log(storedMeetings);
     if (storedMeetings) {
       setMeetings(storedMeetings);
+      const mappedMeetings = meetings.map(convertToEventInput);
+      console.log(mappedMeetings);
+      const eventSourcesm = [
+        {
+          events: mappedMeetings,
+        },
+      ];
+      setEventSources(eventSourcesm);
+      console.log(eventSourcesm);
     }
-
-    const eventSourcesm = [
-      {
-        events: meetings.map(convertToEventInput),
-        color: "purple",
-        textColor: "white",
-      },
-    ];
-    setEventSources(eventSourcesm);
-    console.log(eventSourcesm);
   }, []);
 
-  function handleEventClick(info: any) {
-    console.log(info.event.title);
-  }
-
-  function handleEventDrop(info: any) {
-    console.log(info.event.title, info.event.start);
-    // save updated meeting to API or local storage
-    const updatedMeetings = meetings.map((meeting) =>
-      meeting.id === info.event.id
-        ? { ...meeting, start: info.event.start, end: info.event.end }
-        : meeting
-    );
-    setMeetings(updatedMeetings);
-    localStorage.setItem("meetings", JSON.stringify(updatedMeetings));
-  }
-
   function convertToEventInput(meeting: Meeting): EventInput {
+    console.log(meeting);
     const durationMinutes = 60; // set the duration in minutes
     const start = new Date(meeting.finalTime); // convert the start time to a Date object
 
     const end = new Date(start); // create a new Date object with the same value as start
     end.setMinutes(start.getMinutes() + durationMinutes); // add the duration to the minutes value of the end time
+    console.log(meeting);
     return {
       id: String(meeting.id),
       title: meeting.title,
@@ -59,22 +68,30 @@ function MeetingCalendar(props: any) {
       allDay: false,
     };
   }
+  const handleExportClick = () => {
+    console.log("Export button clicked");
+  };
+  function handleEventClick(info: any) {
+    console.log(info.event.title);
+  }
 
   return (
-    <div className="meeting-calendar">
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek",
-        }}
-        initialView="timeGridWeek"
-        selectable={true}
-        eventSources={eventSources}
-        eventClick={handleEventClick}
-      />
-    </div>
+    <ThemeProvider theme={MyTheme}>
+      <div className={classes.calendarContainer}>
+        <FullCalendar
+          height="600px"
+          themeSystem="bootstrap"
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          selectable={true}
+          eventSources={eventSources}
+          eventClick={handleEventClick}
+        />
+        <Button onClick={handleExportClick} sx={{ mt: 4, mb: 8 }}>
+          Export calendar
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- Front/recruit-front/src/Pages/UserMeetings.tsx
+++ Front/recruit-front/src/Pages/UserMeetings.tsx
@@ -8,7 +8,66 @@ import MeetingListItem from "../Components/Meetings/MeetingListItem";
 import MeetingCalendar from "../Components/Meetings/MeetingsCalendar";
 
 const UserMeetings = (props: any) => {
-  const [meetings, setMeetings] = React.useState<Meeting[]>([]);
+  const [meetings, setMeetings] = React.useState<Meeting[]>([
+    {
+      id: 1,
+      title: "Team Meeting",
+      description: "Discuss team progress",
+      finalTime: "2023-03-01T09:30:00Z",
+      duration: 60,
+      schedulingUrl: "https://randomurl.com",
+      isFinalTime: false,
+      attendees: ["rugile.karengaite@nordsec.com", "blablabla@gmail.com"],
+      meetingTimes: [
+        {
+          id: 1,
+          time: "2023-03-01T09:30:00Z",
+          selectedAttendees: ["Bob", "Alice"],
+        },
+        {
+          id: 2,
+          time: "2023-03-01T09:30:00Z",
+          selectedAttendees: [],
+        },
+        {
+          id: 3,
+          time: "2023-03-01T09:30:00Z",
+          selectedAttendees: ["Bob"],
+        },
+      ],
+      dateString: "",
+      timeString: "",
+    },
+    {
+      id: 2,
+      title: "Team Meeting",
+      description: "Discuss team progress",
+      finalTime: "2023-03-01T09:30:00Z",
+      duration: 60,
+      schedulingUrl: "https://randomurl.com",
+      isFinalTime: true,
+      attendees: ["rugile.karengaite@nordsec.com", "blablabla@gmail.com"],
+      meetingTimes: [
+        {
+          id: 1,
+          time: "2023-03-02T09:30:00Z",
+          selectedAttendees: ["Bob", "Alice"],
+        },
+        {
+          id: 1,
+          time: "2023-03-01T09:30:00Z",
+          selectedAttendees: [],
+        },
+        {
+          id: 1,
+          time: "2023-03-01T09:30:00Z",
+          selectedAttendees: ["Bob"],
+        },
+      ],
+      dateString: "",
+      timeString: "",
+    },
+  ]);
 
   const navigate = useNavigate();
 
@@ -106,6 +165,7 @@ const UserMeetings = (props: any) => {
       <Box
         sx={{
           display: "flex",
+          background: "red",
           justifyContent: "center",
           mb: 5,
           mt: 4,
@@ -161,8 +221,16 @@ const UserMeetings = (props: any) => {
           </Typography>
         </Stack>
       )}
-
-      <MeetingCalendar meetings={meetings}></MeetingCalendar>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          mb: 8,
+          mt: 4,
+        }}
+      >
+        <MeetingCalendar meetings={meetings}></MeetingCalendar>
+      </Box>
     </ThemeProvider>
   );
 };
