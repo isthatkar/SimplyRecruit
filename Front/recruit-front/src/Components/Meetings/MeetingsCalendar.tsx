55caa0d883aaa87b31678110eef6b0fdbb646ed2
Rugile Karengaite <rugile.karengaite@ktu.edu>
2023-04-11 20:55:20 +0300

    export calendar

--- Front/recruit-front/src/Components/Meetings/MeetingsCalendar.tsx
+++ Front/recruit-front/src/Components/Meetings/MeetingsCalendar.tsx
@@ -8,6 +8,7 @@ import { Meeting } from "../../Types/types";
 import { Button } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import { Theme, useStyles } from "../../Styles/Theme";
+import * as ics from "ics";
 
 interface CalendarProps {
   meetings: Meeting[];
@@ -17,10 +18,10 @@ const MeetingCalendar = ({ meetings }: CalendarProps) => {
   const classes = useStyles();
 
   const [eventSources, setEventSources] = useState<EventSourceInput[]>();
+  const [upcomingMeetings, setUpcomingMeetings] = useState<Meeting[]>();
 
   useEffect(() => {
-    const storedMeetings = meetings;
-    const mappedMeetings = storedMeetings
+    const mappedMeetings = meetings
       .filter((m) => !m.isCanceled && m.isFinalTime) //only not canceled and final time meetings
       .map(convertToEventInput);
     const eventSourcesm = [
@@ -28,6 +29,7 @@ const MeetingCalendar = ({ meetings }: CalendarProps) => {
         events: mappedMeetings,
       },
     ];
+    setUpcomingMeetings(meetings.filter((m) => !m.isCanceled && m.isFinalTime));
     setEventSources(eventSourcesm);
   }, [meetings]);
 
@@ -45,9 +47,60 @@ const MeetingCalendar = ({ meetings }: CalendarProps) => {
   };
   const handleExportClick = () => {
     console.log("Export button clicked");
+    downloadICal();
   };
+
   function handleEventClick(info: any) {
     console.log(info.event.title);
+  }
+
+  function downloadICal() {
+    if (upcomingMeetings) {
+      const events = upcomingMeetings.map((meeting) => {
+        const start = new Date(meeting.finalTime);
+        const end = new Date(start);
+        end.setMinutes(start.getMinutes() + meeting.duration);
+
+        return {
+          title: meeting.title,
+          start: [
+            start.getFullYear(),
+            start.getMonth() + 1,
+            start.getDate(),
+            start.getHours(),
+            start.getMinutes(),
+          ],
+          end: [
+            end.getFullYear(),
+            end.getMonth() + 1,
+            end.getDate(),
+            end.getHours(),
+            end.getMinutes(),
+          ],
+          description: meeting.description,
+        };
+      });
+
+      const { error, value } = ics.createEvents(events as any);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const blob = new Blob([value as any], {
+        type: "text/calendar;charset=utf-8",
+      });
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "calendar.ics";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    }
   }
 
   return (
